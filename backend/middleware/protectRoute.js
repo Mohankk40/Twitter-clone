e5fe6addc264dde049adfc108a0bda4395ac7bdd
Mohana Krishnan <mohankk40@gmail.com>
2025-05-22 16:44:58 +0530

    Auth controllers added

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
 export const protectRoute = async (req, res, next) => {
   try {
@@ -13,12 +14,16 @@ export const protectRoute = async (req, res, next) => {
       return res.status(401).json({ error: "Unauthorized: Invalid Token"});
     }
 
-    const user = await User.findById(decoded.userID).select("-password");
+    const user = await User.findById(decoded.userId).select("-password");
 
     if(!user) {
-      return res.status(401).json({ error: "User is not found"});
+      return res.status(404).json({ error: "User is not found"});
     }
-  } catch (error) {
 
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("Error in protectRoute middleware", error.message);
+    return res.status(500).json({ error: "Internal server error"});
   }
 };
